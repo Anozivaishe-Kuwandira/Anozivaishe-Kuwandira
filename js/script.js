68ee1600282c41dd50204383b548ef396f8f7fdf
Anozivaishe Kuwandira <anozivaishekuwandira@gmail.com>
2021-11-10 15:39:30 +0200

    Anozivaishe-Kuwandira

---
+++ js/script.js
@@ -0,0 +1,55 @@
+let menu = document.querySelector('#menu-bars');
+let header = document.querySelector('header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+let cursor1 = document.querySelector('.cursor-1');
+let cursor2 = document.querySelector('.cursor-2');
+
+window.onmousemove = (e) =>{
+    cursor1.style.top = e.pageY + 'px';
+    cursor1.style.left = e.pageX + 'px';
+    cursor2.style.top = e.pageY + 'px';
+    cursor2.style.left = e.pageX + 'px';
+}
+
+document.querySelectorAll('a').forEach(links =>{
+
+    links.onmouseenter = () =>{
+        cursor1.classList.add('active');
+        cursor2.classList.add('active');
+    }
+
+    links.onmouseleave = () =>{
+        cursor1.classList.remove('active');
+        cursor2.classList.remove('active');
+    }
+
+});
+
+AOS.int();
+
+// Typo
+new TypeIt("#type1", {
+    speed: 120,
+    loop: true,
+    waitUntilVisible: true,
+  })
+    .type("Designer", { delay: 500 })
+    .pause(500)
+    .delete(9)
+    .type("Developer", { delay: 500 })
+    .pause(600)
+    .delete(9)
+    .type("Content Creator", { delay: 450 })
+    .pause(500)
+    .delete(15)
+    .go();
